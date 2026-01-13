5eee02219c4b3c0401959cc7c6dfc6c580ff03c1
Ruslan <ibrmov.rusl@mail.ru>
2026-01-13 05:23:20 +0500

    test: add unit tests for lab9 and coverage script

---
+++ Lab9_classes_and_objects/Geometry.Tests/TriangleArrayTest.cs
@@ -0,0 +1,153 @@
+﻿using Geometry.Domain;
+
+namespace Geometry.Tests
+{
+    public class TriangleArrayTests
+    {
+        // ------------------ Конструктор без параметров ------------------
+        [Fact]
+        public void DefaultConstructor_CreatesEmptyArray()
+        {
+            var array = new TriangleArray();
+            Assert.Equal(0, array.Length);
+        }
+
+        // ------------------ Конструктор с размером ------------------
+        [Fact]
+        public void SizeConstructor_ZeroSize_CreatesEmptyArray()
+        {
+            var array = new TriangleArray(0);
+            Assert.Equal(0, array.Length);
+        }
+
+        [Fact]
+        public void SizeConstructor_NegativeSize_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new TriangleArray(-5));
+        }
+
+        [Fact]
+        public void SizeConstructor_PositiveSize_CreatesArray()
+        {
+            int size = 3;
+            var array = new TriangleArray(size);
+
+            Assert.Equal(size, array.Length);
+            for (int i = 0; i < size; i++)
+            {
+                Assert.True(array[i]); // implicit operator bool
+                Assert.InRange(array[i].A, 1, 10);
+                Assert.InRange(array[i].B, 1, 10);
+                Assert.InRange(array[i].C, 1, 10);
+            }
+        }
+
+        // ------------------ Конструктор с фабрикой ------------------
+        [Fact]
+        public void FactoryConstructor_ZeroSize_CreatesEmptyArray()
+        {
+            var array = new TriangleArray(0, () => new Triangle(3, 4, 5));
+            Assert.Equal(0, array.Length);
+        }
+
+        [Fact]
+        public void FactoryConstructor_NegativeSize_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new TriangleArray(-2, () => new Triangle(3, 4, 5)));
+        }
+
+        [Fact]
+        public void FactoryConstructor_NullFactory_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new TriangleArray(3, null));
+        }
+
+        [Fact]
+        public void FactoryConstructor_PositiveSize_CreatesArray()
+        {
+            int size = 2;
+            var array = new TriangleArray(size, () => new Triangle(3, 4, 5));
+
+            Assert.Equal(size, array.Length);
+            for (int i = 0; i < size; i++)
+            {
+                var t = array[i];
+                Assert.Equal(3, t.A);
+                Assert.Equal(4, t.B);
+                Assert.Equal(5, t.C);
+            }
+        }
+
+        // ------------------ Индексатор ------------------
+        [Fact]
+        public void Indexer_GetSet_WorksCorrectly()
+        {
+            var array = new TriangleArray(2, () => new Triangle(1, 1, 1));
+
+            array[0] = new Triangle(3, 4, 5);
+            var t = array[0];
+            Assert.Equal(3, t.A);
+            Assert.Equal(4, t.B);
+            Assert.Equal(5, t.C);
+        }
+
+        [Fact]
+        public void Indexer_Get_InvalidIndex_ThrowsArgumentException()
+        {
+            var array = new TriangleArray(2, () => new Triangle(1, 1, 1));
+            Assert.Throws<ArgumentException>(() => _ = array[-1]);
+            Assert.Throws<ArgumentException>(() => _ = array[2]);
+        }
+
+        [Fact]
+        public void Indexer_Set_InvalidIndex_ThrowsArgumentException()
+        {
+            var array = new TriangleArray(2, () => new Triangle(1, 1, 1));
+            Assert.Throws<ArgumentException>(() => array[-1] = new Triangle(2, 2, 2));
+            Assert.Throws<ArgumentException>(() => array[2] = new Triangle(2, 2, 2));
+        }
+
+        // ------------------ Print ------------------
+        [Fact]
+        public void Print_EmptyArray_PrintsMessage()
+        {
+            var array = new TriangleArray();
+            var originalOut = Console.Out;
+            try
+            {
+                using var writer = new StringWriter();
+                Console.SetOut(writer);
+
+                array.Print();
+
+                string output = writer.ToString();
+                Assert.Contains("Array is empty", output);
+            }
+            finally
+            {
+                Console.SetOut(originalOut);
+            }
+        }
+
+        [Fact]
+        public void Print_NonEmptyArray_PrintsTriangles()
+        {
+            var array = new TriangleArray(2, () => new Triangle(3, 4, 5));
+            var originalOut = Console.Out;
+            try
+            {
+                using var writer = new StringWriter();
+                Console.SetOut(writer);
+
+                array.Print();
+
+                string output = writer.ToString();
+                Assert.Contains("a=3, b=4, c=5", output);
+            }
+            finally
+            {
+                Console.SetOut(originalOut);
+            }
+        }
+    }
+}
---
+++ Lab9_classes_and_objects/Geometry.Tests/TriangleTests.cs
@@ -0,0 +1,176 @@
+﻿using Geometry.Domain;
+
+namespace Geometry.Tests
+{
+    public class TriangleTests
+    {
+        // ------------------ Конструктор ------------------
+
+        [Fact]
+        public void Constructor_ValidSides_CreatesTriangle()
+        {
+            var triangle = new Triangle(3, 4, 5);
+
+            Assert.Equal(3, triangle.A);
+            Assert.Equal(4, triangle.B);
+            Assert.Equal(5, triangle.C);
+        }
+
+        [Theory]
+        [InlineData(0, 4, 5)]
+        [InlineData(-3, 4, 5)]
+        [InlineData(3, 0, 5)]
+        public void Constructor_NonPositiveSide_Throws(double a, double b, double c)
+        {
+            Assert.Throws<ArgumentException>(() => new Triangle(a, b, c));
+        }
+
+        // ------------------ Площадь ------------------
+
+        [Fact]
+        public void GetArea_ValidTriangle_ReturnsCorrectArea()
+        {
+            var triangle = new Triangle(3, 4, 5);
+
+            double area = triangle.GetArea();
+
+            Assert.Equal(6, area);
+        }
+
+        [Fact]
+        public void GetArea_NonExistentTriangle_ReturnsMinusOne()
+        {
+            var triangle = new Triangle(10, 1, 1);
+
+            double area = triangle.GetArea();
+
+            Assert.Equal(-1, area);
+        }
+
+        [Fact]
+        public void StaticGetArea_ValidTriangle_ReturnsCorrectArea()
+        {
+            double area = Triangle.GetArea(3, 4, 5);
+
+            Assert.Equal(6, area);
+        }
+
+        [Fact]
+        public void StaticGetArea_InvalidTriangle_ReturnsMinusOne()
+        {
+            double area = Triangle.GetArea(1, 2, 10);
+
+            Assert.Equal(-1, area);
+        }
+
+        // ------------------ ObjectCount ------------------
+
+        [Fact]
+        public void ObjectCount_IncreasesWhenTriangleCreated()
+        {
+            int before = Triangle.ObjectCount;
+
+            var t1 = new Triangle(3, 4, 5);
+            var t2 = new Triangle(5, 6, 7);
+
+            Assert.Equal(before + 2, Triangle.ObjectCount);
+        }
+
+        // ------------------ Унарные операторы ------------------
+
+        [Fact]
+        public void IncrementOperator_IncreasesAllSides()
+        {
+            var t = new Triangle(3, 4, 5);
+
+            t++;
+
+            Assert.Equal(4, t.A);
+            Assert.Equal(5, t.B);
+            Assert.Equal(6, t.C);
+        }
+
+        [Fact]
+        public void DecrementOperator_DecreasesAllSides()
+        {
+            var t = new Triangle(5, 6, 7);
+
+            t--;
+
+            Assert.Equal(4, t.A);
+            Assert.Equal(5, t.B);
+            Assert.Equal(6, t.C);
+        }
+
+        // ------------------ Бинарные операторы ------------------
+
+        [Fact]
+        public void ComparisonOperators_WorkCorrectly()
+        {
+            var t1 = new Triangle(3, 4, 5);   // area = 6
+            var t2 = new Triangle(6, 7, 8);   // area > 6
+
+            Assert.True(t1 <= t2);
+            Assert.False(t1 >= t2);
+        }
+
+        // ------------------ Приведение типов ------------------
+
+        [Fact]
+        public void ExplicitDoubleConversion_ReturnsArea()
+        {
+            Assert.Equal(6, (double)new Triangle(3, 4, 5));
+
+            Assert.Equal(-1, (double)new Triangle(1, 2, 10));
+        }
+
+        [Fact]
+        public void ImplicitBoolConversion_ValidTriangle_ReturnsTrue()
+        {
+            var triangle = new Triangle(3, 4, 5);
+
+            bool exists = triangle;
+
+            Assert.True(exists);
+        }
+
+        [Fact]
+        public void ImplicitBoolConversion_InvalidTriangle_ReturnsFalse()
+        {
+            var triangle = new Triangle(10, 1, 1);
+
+            bool exists = triangle;
+
+            Assert.False(exists);
+        }
+
+        // ------------------ Random triangle ------------------
+
+        [Fact]
+        public void CreateRandomTriangle_CreatesValidTriangle()
+        {
+            var triangle = Triangle.CreateRandomTriangle(5, 100);
+
+            Assert.True(triangle);
+            Assert.True(triangle.A > 0);
+            Assert.True(triangle.B > 0);
+            Assert.True(triangle.C > 0);
+        }
+
+        [Theory]
+        [InlineData(0, 10)]
+        [InlineData(-5, 10)]
+        [InlineData(10, 10)]
+        [InlineData(15, 10)]
+        public void CreateRandomTriangle_InvalidRange_ThrowsArgumentException(int min, int max)
+        {
+            Assert.Throws<ArgumentException>(() => Triangle.CreateRandomTriangle(min, max));
+        }
+
+        [Fact]
+        public void CreateRandomTriangle_InvalidRange_Throws()
+        {
+            Assert.Throws<ArgumentException>(() => Triangle.CreateRandomTriangle(5, 5));
+        }
+    }
+}
